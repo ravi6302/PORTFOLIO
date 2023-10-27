5e23cfd428275762c5a2ea1ffac8849a513f8f1f
RAVI KUMAR GADALA <130133051+ravi6302@users.noreply.github.com>
2023-10-27 20:50:45 +0530

    Update time.js

--- time.js
+++ time.js
@@ -1,18 +1,18 @@
 
-    const time = document.getElementById("time");
+const time = document.getElementById("time");
 
-    const weeks = ["sun", "mon", "tue", "wed", "thur", "fri", "sat"];
-    const months = ["janu", "feb", "march", "appr", "may", "june", "julay", "aug", "sep", "oct", "nov", "dec"];
-    const date = new Date();
-    const week = date.getDay(); //it has 0 index because today sunday.  the index value is changesd based on current day; it displays the index position of the days
-    console.log(weeks[week]); //here we just asking the index number position contained the value like weeks[5] week=5 we storing the index value in the week variable.
+const weeks = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const months = ["Janu", "Feb", "Mar", "Appr", "May", "June", "Julay", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const date = new Date();
+const week = date.getDay(); //it has 0 index because today sunday.  the index value is changesd based on current day; it displays the index position of the days
+console.log(weeks[week]); //here we just asking the index number position contained the value like weeks[5] week=5 we storing the index value in the week variable.
 
-    const month = date.getMonth(); //it displays the index position of months
-    console.log(months[month]);
-    const Dates = date.getDate();
-    console.log(Dates);
-    const year = date.getFullYear();
-    console.log(year);
-    const sentence = `${weeks[week]},${Dates} ${months[month]} ${year}`;
-    console.log(sentence);
-    time.innerHTML = `${sentence}`;
+const month = date.getMonth(); //it displays the index position of months
+console.log(months[month]);
+const Dates = date.getDate();
+console.log(Dates);
+const year = date.getFullYear();
+console.log(year);
+const sentence = `${weeks[week]}, ${months[month]} ${Dates} ${year}`;
+console.log(sentence);
+time.innerHTML = `${sentence}`;
